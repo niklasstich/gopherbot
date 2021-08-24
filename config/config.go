f78bca80c4083b9daf212ddc197a91a22deec9d5
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-08-24 22:07:28 +0200

    refactored configuration to use the viper configuration framework, create default config file if none is found

--- config/config.go
+++ config/config.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"github.com/BurntSushi/toml"
-	"log"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
 )
 
 type config struct {
@@ -25,7 +26,57 @@ var Conf config
 
 func init() {
 	//ignore metadata
-	if _, err := toml.DecodeFile("config/config.toml", &Conf); err != nil {
-		log.Fatal("Failed to parse config file: ", err.Error())
+	viper.SetDefault("Discord", discordConfig{
+		BotToken:                 "",
+		GuildId:                  "",
+		ClearSlashCommandsOnQuit: true,
+	})
+	viper.SetDefault("Application", applicationConfig{
+		PingpongMessage: "Ping Pong!",
+		DailyCurrency:   200,
+	})
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
+	viper.AddConfigPath("$home/.config/gopherbot")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok { //create default conf in default location
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Fatal("Failed to get working directory while creating default config file: ", err.Error())
+			}
+			confdir := wd + "/config"
+			fp := confdir + "/config.yaml"
+			log.Warnf("Couldn't find configuration file, creating new one at %s with default values", fp)
+			err = os.MkdirAll(confdir, 0700)
+			if err != nil {
+				log.Fatalf("Failed to create directory %s: %v", confdir, err)
+			}
+			err = viper.SafeWriteConfigAs(fp)
+			if err != nil {
+				if _, ok := err.(viper.ConfigFileAlreadyExistsError); ok {
+					log.Fatalf("Failed to find config file, but config file in default location %s aready exists. Contradictory, bailing", fp)
+				} else {
+					log.Fatalf("Failed to create config file in default location %s: %v", fp, err.Error())
+				}
+			}
+		} else {
+			log.Fatal("Failed to read config file: ", err.Error())
+		}
+	}
+
+	if err := viper.Unmarshal(&Conf); err != nil {
+		log.Fatalf("Failed to unmarshal config into Conf struct, %v", err)
+	}
+
+	//log configuration details
+	if Conf.Discord.BotToken == "" {
+		log.Fatal("No application token for the bot set. Please fill out the appropriate configuration file.")
+	}
+
+	if Conf.Discord.GuildId != "" {
+		log.Infof("Running in guild mode for GuildID %s", Conf.Discord.GuildId)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/niklasstich/gopherbot
 go 1.16
 
 require (
-	github.com/BurntSushi/toml v0.3.1
-	github.com/bwmarrin/discordgo v0.23.3-0.20210627161652-421e14965030
-	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	github.com/bwmarrin/discordgo v0.23.3-0.20210821175000-0fad116c6c2a
+	github.com/mattn/go-sqlite3 v1.14.7
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/viper v1.8.1
 )
