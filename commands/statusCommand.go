639a891d2b60943e8bbec21b69127375b5ecea36
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-08-28 23:31:18 +0200

    expand status command output

--- commands/statusCommand.go
+++ commands/statusCommand.go
@@ -3,12 +3,14 @@ package commands
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"github.com/niklasstich/gopherbot/config"
 	"github.com/niklasstich/gopherbot/userdata"
 )
 
 var StatusCommand = discordgo.ApplicationCommand{
 	ID:            "",
 	ApplicationID: "",
+	Type:          discordgo.ChatApplicationCommand,
 	Name:          "status",
 	Description:   "Status of bot and debug information",
 	Version:       "1",
@@ -25,6 +27,9 @@ func StatusHandler(s *discordgo.Session, interact *discordgo.InteractionCreate) 
 
 	statString := fmt.Sprintf("Currently running on %d servers, serving %d users.\nThis is shard %d; %d shard(s) total.\n"+
 		"%d Users in DB.", guilds, users, s.ShardID, s.ShardCount, userdata.UserCount())
+	if config.Conf.Discord.GuildId != "" {
+		statString += fmt.Sprintf("\nRunning in Guild Debug mode on GuildID %s", config.Conf.Discord.GuildId)
+	}
 	sendInteractionResponse(s, interact, &discordgo.InteractionResponseData{
 		Content: statString,
 	})
